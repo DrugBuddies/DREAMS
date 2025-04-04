5723e1705decf62dd0d21fb363ed682a996e7dd2
jerko <riccardo.arn@gmail.com>
2025-04-04 12:56:58 +0200

    second commit

---
+++ install_geneformer.sh
@@ -0,0 +1,4 @@
+git lfs install
+git clone https://huggingface.co/ctheodoris/Geneformer
+cd Geneformer
+pip install .
--- lib/data_manager.py
+++ lib/data_manager.py
@@ -1,7 +1,7 @@
 import torch
 from datasets import load_from_disk
 from geneformer import TOKEN_DICTIONARY_FILE
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from geneformer import TranscriptomeTokenizer
 import pickle as pkl
 import geneformer.perturber_utils as pu
@@ -10,41 +10,60 @@ import json
 from transformers import AutoTokenizer
 
 
+def tokenize_h5ad(in_dir, out_dir, file_format='h5ad', key_name = 'cell_line'):
+    """
+    convert single cell h5ad dataset  to dataset compatible with Dreams
+    :param in_dir: path to h5ad dataset
+    :param out_dir: path to save dataset
+    :param file_format: h5ad or loom
+    :param key_name:  name of the key to be added
+    :return:
+    """
+    tok = TranscriptomeTokenizer({key_name:key_name}, special_token=True)
+    tok.tokenize_data(in_dir, out_dir, "temp", file_format=file_format)
+
+def sample_pairs(drug_data, cell_data, n_pairs_per_sample):
+    # TODO this function should build cell-drug pairs
+    return []
 
 def perturb(input_data, perturbation, token_dict):
+    """
+    perturb a tokenized cell
+    :param input_data: tokenized cell data
+    :param perturbation: dict that maps gene to perturbation type
+    :param token_dict: dictionary of the geneformer
+    :return:
+    """
     perturbed = input_data.copy()
-    for i in range(len(input_data['input_ids'])):
-        for k in perturbation.keys():
-            gene_token = token_dict[k]
-            to_append = torch.tensor([gene_token],dtype=torch.int)
-            perturbed['input_ids'][i] = perturbed['input_ids'][i][ perturbed['input_ids'][i] != gene_token]
-            if perturbation[k] == 'overexpress':
-                perturbed['input_ids'][i] = torch.concat(( perturbed['input_ids'][i][:1], # cls token
-                                                           to_append ,   # gene to overexpress
-                                                           perturbed['input_ids'][i][1:])) # other genes
+    for k in perturbation.keys():
+        if k not in token_dict.keys():
+            continue
+        gene_token = token_dict[k]
+        to_append = torch.tensor([gene_token],dtype=torch.int)
+        perturbed['input_ids'] = perturbed['input_ids'][ perturbed['input_ids'] != gene_token]
+        if perturbation[k] == 'agonist':
+            perturbed['input_ids'] = torch.concat(( perturbed['input_ids'][:1], # cls token
+                                                       to_append ,   # gene to overexpress
+                                                           perturbed['input_ids'][1:])) # other genes
+    perturbed['length'] = torch.tensor(len(perturbed['input_ids']))
     return perturbed
 
 
-class DREAMSDataLoader(DataLoader):
+class DREAMSDataset(Dataset):
 
-    def __init__(self, transcriptome_path,
+    def __init__(self,
                  drugs_data_path,
                  model_input_size,
                  temp_dir='data/temp',
-                 file_format='h5ad'):
-
-
-        # load and tokenize transcriptomics data
-        tok = TranscriptomeTokenizer({'tissue':'tissue'}, special_token=True)
-        tok.tokenize_data(transcriptome_path, temp_dir, "temp", file_format=file_format)
-
+                 file_format='h5ad',
+                 n_pairs_per_sample=1):
 
         with open(TOKEN_DICTIONARY_FILE, "rb") as f:
             self.gene_token_dict = pkl.load(f)
 
         self.model_input_size = model_input_size
-        self.dataset = load_from_disk(temp_dir)
-        self.dataset.set_format('torch')
+        self.transcriptome = load_from_disk(temp_dir)
+        self.transcriptome.set_format('torch')
         self.pad_token_id = self.gene_token_dict.get("<pad>")
 
         with open(drugs_data_path, "r") as f:
@@ -52,44 +71,31 @@ class DREAMSDataLoader(DataLoader):
 
         self.drug_tokenizer = AutoTokenizer.from_pretrained("ibm/MoLFormer-XL-both-10pct", trust_remote_code=True)
 
-        self.indices = []  #  ( i, 'id')
+        self.indices = sample_pairs(self.transcriptome, self.drugs, n_pairs_per_sample)
 
-
-        # TODO: sposta qua codice per preprocessare
-        # TODO: compute pairs cell-drug based on tissue
-        # TODO:
 
     def __len__(self):
         return len(self.indices)
 
     def __getitem__(self, index):
         cell_idx, drug_id = self.indices[index]
-        cell = self.dataset[cell_idx]
-        smile = self.drugs[drug_id]['smile']
-        perturbation = perturb(cell, self.drugs[drug_id]['perturbations'], self.gene_token_dict )
+        cell = self.transcriptome[cell_idx]
+        smile = self.drugs[drug_id]['SMILES']
+        perturbation = perturb(cell, self.drugs[drug_id]['targets'], self.gene_token_dict )
         return cell, perturbation, smile
 
     def tokenize_cells(self, cells):
         input_batch = cells['input_ids']
-        max_len = int(max(cells["length"]))
+        max_len = max(cells["length"])
         input_batch =  pu.pad_tensor_list(
             input_batch, max_len, self.pad_token_id, self.model_input_size
         )
-        attn = pu.gen_attention_mask(input_batch, max_len)
+
+        attn = pu.gen_attention_mask(cells, max_len)
         return input_batch, attn
 
     def tokenize_drugs(self, smiles):
-        smile_inputs = self.drug_tokenizer(smiles, padding=True, return_tensors="pt")
-        return smile_inputs['input_ids'], smile_inputs['attention_mask']
+        return self.drug_tokenizer(smiles, padding=True, return_tensors="pt")
+        #smile_inputs = self.drug_tokenizer(smiles, padding=True, return_tensors="pt")
+        #return smile_inputs['input_ids'], smile_inputs['attention_mask']
 
-
-    def sample_pairs(self):
-        # TODO this function should build cell-drug pairs
-        pass
-
-
-def sample_example(idx, dataset):
-    c,p,d =  dataset[idx]
-    c_tok = dataset.tokenize_cells(c)
-    p_tok = dataset.tokenize_cells(p)
-    d_tok = dataset.tokenize_drugs(d)
