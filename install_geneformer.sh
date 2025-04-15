120c8f7fb38683400fe9a833c91611a47d1e0a0f
Riccardo Arnese <42966046+j3rk0@users.noreply.github.com>
2025-04-15 14:28:17 +0200

    Update install_geneformer.sh

--- install_geneformer.sh
+++ install_geneformer.sh
@@ -1,3 +1,4 @@
+pip install "ray[default]@https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-3.0.0.dev0-cp313-cp313-manylinux2014_x86_64.whl" # only for python 3.13
 git lfs install
 git clone https://huggingface.co/ctheodoris/Geneformer
 cd Geneformer
